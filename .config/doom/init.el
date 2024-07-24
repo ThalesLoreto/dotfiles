3d4254bf32bf401ea1cda6c5e5ae70b5a6d6d66e
ThalesLoreto <tloreto.dev@gmail.com>
2024-07-24 17:10:16 -0300

    added docker fmt, js&lua now using lsp+ts and removed sh package

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -77,7 +77,7 @@
        ;;collab            ; buffers with friends
        (debugger +lsp)     ; FIXME stepping through code, to help you add bugs
        ;;direnv
-       ;;docker
+       docker              ; container
        ;;editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
@@ -133,13 +133,15 @@
        ;;idris             ; a language you can depend on
        json                ; At least it ain't XML
        ;;(java +lsp)       ; the poster child for carpal tunnel syndrome
-       javascript          ; all(hope(abandon(ye(who(enter(here))))))
+       (javascript +lsp          ; all(hope(abandon(ye(who(enter(here))))))
+                   +tree-sitter)
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        ;;latex             ; writing papers in Emacs has never been so fun
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
-       lua                 ; one-based indices? one-based indices
+       (lua +lsp
+            +tree-sitter)  ; one-based indices? one-based indices
        ;;markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
@@ -161,7 +163,7 @@
        (rust +lsp)         ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;(scheme +guile)   ; a fully conniving family of lisps
-       sh                  ; she sells {ba,z,fi}sh shells on the C xor
+       ;;sh                ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
