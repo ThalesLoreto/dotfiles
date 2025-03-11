330ab247d8f6fd66237b7e54685940ceaa9c9473
ThalesLoreto <tloreto.dev@gmail.com>
2025-03-11 02:26:03 -0300

    custom treesitter

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "rust",
+        "go",
+        "python",
+        "typescript",
+        "javascript",
+      },
+    },
+  },
+}
